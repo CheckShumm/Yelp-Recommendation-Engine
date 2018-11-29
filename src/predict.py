857becb8112854efa9ab0a0b6b955d9536124101
CheckShumm <checkshumm@gmail.com>
2018-11-28 19:06:07 -0500

    parse_json added

--- src/predict.py
+++ src/predict.py
@@ -7,7 +7,7 @@ import math
 
 # SparkSQL imports
 from pyspark import SparkContext, SparkConf
-from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql import SparkSession, DataFrame, Row
 from pyspark.sql.functions import max, struct, first
 from pyspark.sql.types import DoubleType,IntegerType
 
@@ -19,7 +19,23 @@ from pyspark.ml.feature import OneHotEncoder, StringIndexer
 
 # utils
 from spark.utils import *
+from pprint import pprint
 
+def parse_json(user_id):
+        with open('../data/recs.json') as f:
+            recs = json.load(f)
+
+        conf = SparkConf().setAppName("app").setMaster("local[2]")
+        sc = SparkContext(conf = conf)
+        ss = SparkSession(sc)
+
+        restaurants = sc.parallelize([(k,)+(v,) for k,v in recs.items()]).toDF(['business_id','stars'])
+        user = sc.parallelize([(user_id,) for k in recs.items()]).toDF(['user_id'])
+        data = restaurants.join(user).toDF()
+        print(user.show(5))
+        print(restaurants.show(5))
+        print(data.show(5))
+        
 def predict():
     # init spark session
     ss = spark_init('als')
@@ -44,6 +60,9 @@ def predict():
     print("Creating recommendation for user")
     user_recs = model.recommendForAllUsers(5)
 
+    # save to csv
+    user_recs.toPandas().to_csv("../data/yelp_recs.csv", encoding='utf-8', index=False)
+
     # Convert int ids to original string ids
     recs = {}
     user_id = df.where(df.int_user_id == 148).select('user_id').collect()[0]
@@ -66,7 +85,7 @@ def predict():
         json.dump(recs, fp, sort_keys=True, indent=4)
 
 def __main__():
-    predict()
+    parse_json(148)
 
 if __name__ == '__main__':
     __main__()
